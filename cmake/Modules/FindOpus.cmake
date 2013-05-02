7525020421430ced51c5c5d19e0bf567f3d2f556
ishmal <ishmalius@gmail.com>
2013-05-02 14:43:28 -0500

    New file

---
+++ cmake/Modules/FindOpus.cmake
@@ -0,0 +1,27 @@
+if(NOT OPUS_FOUND)
+  pkg_check_modules (OPUS_PKG opus)
+  find_path(OPUS_INCLUDE_DIR NAMES opus.h
+    PATHS
+    ${OPUS_PKG_INCLUDE_DIRS}
+    /usr/include/opus
+    /usr/local/include/opus
+  )
+
+  find_library(OPUS_LIBRARIES NAMES opus
+    PATHS
+    ${OPUS_PKG_LIBRARY_DIRS}
+    /usr/lib
+    /usr/local/lib
+  )
+
+if(OPUS_INCLUDE_DIR AND OPUS_LIBRARIES)
+  set(OPUS_FOUND TRUE CACHE INTERNAL "Opus found")
+  message(STATUS "Found Opus: ${OPUS_INCLUDE_DIR}, ${OPUS_LIBRARIES}")
+else(OPUS_INCLUDE_DIR AND OPUS_LIBRARIES)
+  set(OPUS_FOUND FALSE CACHE INTERNAL "Opus found")
+  message(STATUS "Opus not found.")
+endif(OPUS_INCLUDE_DIR AND OPUS_LIBRARIES)
+
+mark_as_advanced(OPUS_INCLUDE_DIR OPUS_LIBRARIES)
+
+endif(NOT OPUS_FOUND)
